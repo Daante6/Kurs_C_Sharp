199f112e24e7d2f503a2c4b282ad2446379fb59a
HHH DDD <hubert.dega@gmail.com>
2021-02-10 01:55:26 +0100

    variable format in WriteLine

--- Pierwszy/Program.cs
+++ Pierwszy/Program.cs
@@ -8,6 +8,9 @@ namespace Pierwszy
 		public static void Main(string[] args)
 		{
 			Console.WriteLine("Hello world");
+			Console.WriteLine("Liczba {0} w formacie dziesietnym {0} i szesnastkowym {0:x5} " , 155 );
+			Console.WriteLine("Liczba {0} w formacie walutowym {0:c}" , 155.5 );
+			Console.WriteLine("Liczba {0} w formacie zmienoprzecinkowym {0:f4} " , 155.43666 );
 			Console.ReadLine();
 		}
 	}
